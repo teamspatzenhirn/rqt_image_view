0f6f68e2d972f5e87d48d75668c3c1d4ba42aadc
Dominik <dominik.authaler@uni-ulm.de>
2021-07-20 22:17:44 +0200

    revert changes in ratio layouted frame

--- include/rqt_image_view/ratio_layouted_frame.h
+++ include/rqt_image_view/ratio_layouted_frame.h
@@ -45,68 +45,71 @@
 namespace rqt_image_view {
 
 /**
- * RatioLayoutedFrame is a layout containing a single frame with a fixed aspect
- * ratio. The default aspect ratio is 4:3.
+ * RatioLayoutedFrame is a layout containing a single frame with a fixed aspect ratio.
+ * The default aspect ratio is 4:3.
  */
-    class RatioLayoutedFrame : public QFrame {
+class RatioLayoutedFrame
+  : public QFrame
+{
 
-        Q_OBJECT
+  Q_OBJECT
 
-    public:
-        RatioLayoutedFrame(QWidget *parent, Qt::WindowFlags flags = 0);
+public:
 
-        virtual ~RatioLayoutedFrame();
+  RatioLayoutedFrame(QWidget* parent, Qt::WindowFlags flags = 0);
 
-        const QImage &getImage() const;
+  virtual ~RatioLayoutedFrame();
 
-        QImage getImageCopy() const;
+  const QImage& getImage() const;
 
-        void setImage(const QImage &image);
+  QImage getImageCopy() const;
 
-        QRect getAspectRatioCorrectPaintArea();
+  void setImage(const QImage& image);
 
-        void resizeToFitAspectRatio();
+  QRect getAspectRatioCorrectPaintArea();
 
-        void setOuterLayout(QHBoxLayout *outer_layout);
+  void resizeToFitAspectRatio();
 
-        void setInnerFrameMinimumSize(const QSize &size);
+  void setOuterLayout(QHBoxLayout* outer_layout);
 
-        void setInnerFrameMaximumSize(const QSize &size);
+  void setInnerFrameMinimumSize(const QSize& size);
 
-        void setInnerFrameFixedSize(const QSize &size);
+  void setInnerFrameMaximumSize(const QSize& size);
 
-        signals:
+  void setInnerFrameFixedSize(const QSize& size);
 
-                void delayed_update();
+signals:
 
-        void mouseLeft(int x, int y);
+  void delayed_update();
 
-    protected
-        slots:
+  void mouseLeft(int x, int y);
 
-                void onSmoothImageChanged(bool
-        checked);
+protected slots:
 
-    protected:
-        void setAspectRatio(unsigned short width, unsigned short height);
+  void onSmoothImageChanged(bool checked);
 
-        void paintEvent(QPaintEvent *event);
+protected:
 
-    private:
-        static int greatestCommonDivisor(int a, int b);
+  void setAspectRatio(unsigned short width, unsigned short height);
 
-        void mousePressEvent(QMouseEvent *mouseEvent);
+  void paintEvent(QPaintEvent* event);
 
-        QHBoxLayout *outer_layout_;
+private:
 
-        QSize aspect_ratio_;
+  static int greatestCommonDivisor(int a, int b);
 
-        QImage qimage_;
-        mutable QMutex qimage_mutex_;
+  void mousePressEvent(QMouseEvent * mouseEvent);
 
-        bool smoothImage_;
-    };
+  QHBoxLayout* outer_layout_;
 
-} // namespace rqt_image_view
+  QSize aspect_ratio_;
+
+  QImage qimage_;
+  mutable QMutex qimage_mutex_;
+
+  bool smoothImage_;
+};
+
+}
 
 #endif // rqt_image_view__RatioLayoutedFrame_H
